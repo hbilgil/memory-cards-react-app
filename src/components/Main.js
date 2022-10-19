0945469a184a55a5b840c98f4306628f941f1a00
Halil Bilgil <hbilgil@gmail.com>
2022-10-19 17:57:59 +0300

    Edit Main component to add new elements

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = () => {
+
+    return (
+        <MainWrapper>
+          
+        </MainWrapper>
+    )
+}
+
+const MainWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  padding: 4rem;
+  
+`
+
+export default Main;
