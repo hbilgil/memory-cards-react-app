e28bbee9321785f3fd0a49415f3d48f44bfd1cfa
Halil Bilgil <hbilgil@gmail.com>
2022-10-19 16:12:23 +0300

    Edit App.js to import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-
-function App() {
+const App = () => {
   return (
     <>
-      
+      <Header />
+      <Main/>
+      <Footer />
     </>
-  );
+  )
 }
 
 export default App;
