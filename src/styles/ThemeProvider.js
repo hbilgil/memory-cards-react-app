822a8d37c17ac4c36d9bf64ce797f6fe5d184894
Halil Bilgil <hbilgil@gmail.com>
2022-10-19 17:56:16 +0300

    Edit Theme Provider to add new colors

--- src/styles/ThemeProvider.js
+++ src/styles/ThemeProvider.js
@@ -1,6 +1,11 @@
 const Theme = {
     colors: {
-      dark: "#333"
+      dark: "#333",
+      white: "#ffffff",
+      lightHeader: "#ffffff",
+      lightFooter: "#cce7e8",
+      linkHover: "#D933FF",
+      linkActive: "#FF8433"
     },
 };
   
