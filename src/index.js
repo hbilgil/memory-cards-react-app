484e58982047bb0b957d58c90ce8f8246a860a78
Halil Bilgil <hbilgil@gmail.com>
2022-10-19 15:12:57 +0300

    Edit index.js to import styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from "./styles/GlobalStyle";
+import Theme from "./styles/ThemeProvider";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
