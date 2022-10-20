4b2cd05830ab7897d17f96a12e3964b8a53fed57
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 23:32:52 +0300

    Edit Card component for conditional rendering and change styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,18 +3,21 @@ import styled from 'styled-components';
 import logoCup from '../images/uefachampionsleaguecup.png';
 
 const Card = (props) => {
-    const{ card: { src, name, won }, handleCardClick } = props;
+  const{ card: { src, name, won }, handleCardClick } = props;
 
-    return (
-        <CardWrapper onClick={handleCardClick.bind(this, name)}>
-            <Image src={src} alt={name}/>
-            <Name>{name}</Name>
-            <TitleDiv>
-                <CupLogo src={logoCup} alt="cup-logo"/>
-                <TitleText>x {won} times win</TitleText>
-            </TitleDiv>
-        </CardWrapper>
-    )
+  return (
+    <CardWrapper onClick={handleCardClick.bind(this, name)}>
+      <Image src={src} alt={name}/>
+      <Name>{name}</Name>
+      <TitleDiv>
+        <CupLogo src={logoCup} alt="cup-logo"/>
+        {won > 1
+        ? <TitleText>x {won} times win</TitleText>
+        : <TitleText>x {won} time win</TitleText>
+        }
+      </TitleDiv>
+    </CardWrapper>
+  )
 }
 
 const CardWrapper = styled.div`
@@ -29,36 +32,38 @@ const CardWrapper = styled.div`
   transition: transform 0.3s;
   cursor: pointer;
   opacity: 0.9;
-  margin-bottom: 10px;
+  
   &:hover {
     transform: scale(1.05);
   }
-`
+`;
 
 const Image = styled.img`
   padding: 0.5rem;
   width: 80%;
   height: auto;
-`
+`;
 
 const Name = styled.p`
   padding: 0.5rem;
   font-size: 1.5rem;
   font-weight: 700;
   text-align: center;
-`
+`;
 
 const TitleDiv = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     margin-bottom: 10px;
+    gap: 2px;
     
 `;
 
 const CupLogo = styled.img`
   width: 1rem;
   height: auto;
+  margin-bottom: 2px;
 `;
 
 const TitleText = styled.p`
