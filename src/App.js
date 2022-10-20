6679cff27bf5441fd1ac650ba9a256e0d3d839ea
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 23:34:00 +0300

    Edit App.js to migrate methods from Main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Modal from './components/Modal';
 
 const App = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [teams, setTeams] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleCardClick = (cardName) => {
+    if (!teams.includes(cardName)) {
+      setTeams((prevArr) => [...prevArr, cardName]);
+      setCurrentScore((prevScore) => prevScore + 1);
+    } else {
+      if (currentScore > bestScore) {
+        setBestScore(currentScore);
+      }
+      setMessage("🙁 Game Over! 🙁");
+      setGameOver(true);
+    }
+  };
+
+  const resetGame = () => {
+    setGameOver(false);
+    setCurrentScore(0);
+    setTeams([]);
+  };
+
+  useEffect(() => {
+    if (currentScore === 12) {
+      setMessage("🎉 You Won! 🎉");
+      setBestScore(currentScore);
+      setGameOver(true);
+    }
+  }, [currentScore]);
+
   return (
     <>
+      {gameOver && <Modal message={message} resetGame={resetGame} currentScore={currentScore} />}
       <Header />
-      <Main/>
+      <Main 
+        currentScore={currentScore}
+        bestScore={bestScore}
+        handleCardClick={handleCardClick}
+        />
       <Footer />
     </>
   )
