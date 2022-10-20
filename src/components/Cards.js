598d9b7e79a134c09dc31243f79dae4de6eca7fe
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 23:33:28 +0300

    Edit Cards component for changing styles

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,33 +4,34 @@ import Card from './Card';
 import teamsInfo from './Teams';
 
 const Cards = (props) => {
-    const { handleCardClick, currentScore, bestScore } = props;
+  const { handleCardClick, currentScore, bestScore } = props;
 
-    const [cards, setCards] = useState(teamsInfo);
+  const [cards, setCards] = useState(teamsInfo);
 
-    const shuffleArray = (array) => {
-        return array.sort(() => Math.random() - 0.5)
-    }
+  const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5)
+  }
 
-    useEffect(() => {
-        const newCards = [...cards];
-        shuffleArray(newCards);
-        setCards(newCards);
-    }, [currentScore, bestScore]);
+  useEffect(() => {
+    const newCards = [...cards];
+    shuffleArray(newCards);
+    setCards(newCards);
+  }, [currentScore, bestScore]);
     
-    return (
-        <CardsContainer>
-            {cards.map((card) => (
-            <Card card={card} key={card.name} handleCardClick={handleCardClick} />
-            ))}
-        </CardsContainer>
-    )
+  return (
+    <CardsContainer>
+      {cards.map((card) => (
+      <Card card={card} key={card.name} handleCardClick={handleCardClick} />
+      ))}
+    </CardsContainer>
+  )
 }
 
 const CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 3rem;
+  margin-bottom: 20px;
   @media (max-width: 800px) {
     grid-template-columns: repeat(3, 1fr);
   }
