5cb780f4726f7783915286b0044000a145ac40f6
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 21:10:11 +0300

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import Modal from './components/Modal';
 
-const App = (props) => {
-
-  const {gameOver, message, resetGame, currentScore } = props;
+const App = () => {
   return (
     <>
-      {gameOver && <Modal message={message} resetGame={resetGame} score={currentScore} />}
       <Header />
       <Main/>
       <Footer />
