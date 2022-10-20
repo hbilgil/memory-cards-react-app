3a740afcc62d07840a4f085990c5cdf2a3ae6f44
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 16:29:13 +0300

    Edit App.js to import Modal component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Modal from './components/Modal';
 
 const App = () => {
   return (
     <>
+      <Modal />
       <Header />
       <Main/>
       <Footer />
