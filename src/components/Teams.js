506a2ab5337dc77aaa52d9db23b009fec03387a9
Halil Bilgil <hbilgil@gmail.com>
2022-10-20 20:50:28 +0300

    Add Teams module to define team objects in an array

---
+++ src/components/Teams.js
@@ -0,0 +1,77 @@
+import real from '../images/real.png';
+import milan from '../images/milan.png';
+import bayern from '../images/bayern.png';
+import liverpool from '../images/liverpool.png';
+import barcelona from '../images/barca.png';
+import ajax from '../images/ajax.png';
+import manchester from '../images/manchester.png';
+import inter from '../images/inter.png';
+import juventus from '../images/juve.png';
+import chelsea from '../images/chelsea.png';
+import porto from '../images/porto.png';
+import dortmund from '../images/dortmund.png';
+
+let teamsInfo = [
+    {
+      src: real,
+      name: "Real Madrid",
+      won: "14",
+    },
+    {
+      src: milan,
+      name: "AC Milan",
+      won: "7",
+    },
+    {
+      src: bayern,
+      name: "Bayern Munchen",
+      won: "6",
+    },
+    {
+      src: liverpool,
+      name: "Liverpool",
+      won: "6",
+    },
+    {
+      src: barcelona,
+      name: "Barcelona",
+      won: "5",
+    },
+    {
+      src: ajax,
+      name: "Ajax",
+      won: "4",
+    },
+    {
+      src: manchester,
+      name: "Manchester United",
+      won: "3",
+    },
+    {
+      src: inter,
+      name: "Inter Milan",
+      won: "3",
+    },
+    {
+      src: juventus,
+      name: "Juventus",
+      won: "2",
+    },
+    {
+      src: chelsea,
+      name: "Chelsea",
+      won: "2",
+    },
+    {
+      src: porto,
+      name: "Porto",
+      won: "2",
+    },
+    {
+      src: dortmund,
+      name: "Borussia Dortmund",
+      won: "1",
+    }
+];
+
+export default teamsInfo;
